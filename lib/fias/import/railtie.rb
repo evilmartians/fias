5a377c823f4d615c8315af9d633303c774e121d8
Victor Sokolov <gzigzigzeo@evilmartians.com>
2015-02-10 17:48:43 +0300

    Railtie paths to rake update

--- lib/fias/import/railtie.rb
+++ lib/fias/import/railtie.rb
@@ -1,8 +1,8 @@
 module Fias
   class Railtie < Rails::Railtie
     rake_tasks do
-      load File.join(File.dirname(__FILE__), '../../tasks/db.rake')
-      load File.join(File.dirname(__FILE__), '../../tasks/download.rake')
+      load File.join(File.dirname(__FILE__), '../../../tasks/db.rake')
+      load File.join(File.dirname(__FILE__), '../../../tasks/download.rake')
     end
   end
 end
