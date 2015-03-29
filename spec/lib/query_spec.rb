5ad3cc309dbfdae18beed53d16174a5b4ac06cba
Viktor Sokolov <gzigzigzeo@evilmartians.com>
2015-03-29 05:48:56 +0300

    No spec for query

--- spec/lib/query_spec.rb
+++ spec/lib/query_spec.rb
@@ -12,30 +12,5 @@ describe Fias::Query do
       end
     end
   end
-=begin
-  it 'must not resanitize already sanitized parts' do
-    sanitized = Addressing::Parts.sanitize(
-      city: 'г Краснодар', street: 'Ушинского'
-    )
-    expect(sanitized).to be_present
-    expect(Addressing::Parts.sanitize(sanitized)).to eq(sanitized)
-  end
-
-  it 'must create correct address string from a parts' do
-    address = Addressing::Parts.address_from_parts(
-      house_number: 33,
-      city: 'Санкт-Петербург',
-      street: 'Искровский пр.'
-    )
-    expect(address).to eq('33, проспект Искровский, город Санкт-Петербург')
-  end
-
-  it 'must create correct address string from a full address' do
-    address = Addressing::Parts.address_from_parts(
-      house_number: 33,
-      address: 'Искровский проспект, город Санкт-Петербург'
-    )
-    expect(address).to eq('33, Искровский проспект, город Санкт-Петербург')
-  end
-=end
 end
+
